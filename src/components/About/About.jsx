67adcada8fbdd5dfecae2b49da78ca8d91a3ccab
Abhinav Chauhan <officialabhi549@gmail.com>
2025-09-29 17:13:21 +0530

    profile img updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
 import profileImage from '../../assets/image.jpeg';
+import p from '../../assets/Project_logo/p.png'
 
 const About = () => {
   return (
@@ -72,7 +73,7 @@ const About = () => {
             gyroscope={true}
           >
             <img
-              src={"/src/assets/p.png"}
+              src={p}
               alt="abhinav Chauhan"
               className="w-full h-full rounded-full object-fill drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
             />
