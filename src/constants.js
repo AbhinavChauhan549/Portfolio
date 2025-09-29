2af1ec1718c46b757858a99eb983201833f398c2
Abhinav Chauhan <officialabhi549@gmail.com>
2025-09-29 16:55:07 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,310 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+import streamlit from './assets/tech_logo/streamlit.png';
+import numpy from './assets/tech_logo/numpy.png';
+import pandas from './assets/tech_logo/pandas.png';
+import matplotlib from './assets/tech_logo/matplotlib.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+import cgclogo from './assets/logo/cgc.png';
+import mmlogo from './assets/logo/MM.png'
+
+// Project Section Logo's
+// import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+import heartStroke from './assets/Project_logo/Heart stroke prediction.png'
+import Ochi from './assets/Project_logo/Ochi.png'
+import housePrice from './assets/Project_logo/House Price Predicition.png'
+import tunely from './assets/Project_logo/Tunely.png'
+import mojito from './assets/Project_logo/Mojito.png'
+import riffly from './assets/Project_logo/Riffly Ecommerce.png'
+import amix from './assets/Project_logo/AMIX.png'
+import landingpage from './assets/Project_logo/LandingPage1.png'
+import tip from './assets/Project_logo/Tip.png'
+import rockpaperscissor from './assets/Project_logo/RockpaperScissor.png'
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Streamlit', logo: streamlit },
+
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Python', logo: pythonLogo },
+
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Numpy', logo: numpy },
+      { name: 'Pandas', logo: pandas },
+      { name: 'Matplotlib', logo: matplotlib },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: webverseLogo,
+    role: "Fullstack Developer",
+    company: "Webverse Digital",
+    date: "April 2024 - Present",
+    desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React JS",
+      "TypeScript",
+      "Node JS",
+      "Tailwind CSS",
+      "MongoDb",
+      "Redux",
+      " Next Js",
+    ],
+  },
+  {
+    id: 1,
+    img: agcLogo,
+    role: "Fullstack Engineer",
+    company: "Agumentik Group of Companies",
+    date: "July 2023 - March 2024",
+    desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "JavaScript",
+      "Tailwind CSS",
+      "HTML",
+      "CSS",
+      "SQL",
+    ],
+  },
+  {
+    id: 2,
+    img: newtonschoolLogo,
+    role: "Frontend Intern",
+    company: "Newton School",
+    date: "September 2021 - August 2022",
+    desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+    skills: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Bootstrap",
+      "Figma",
+      "Material UI",
+    ],
+  },
+];
+
+export const education = [
+
+  {
+    id: 1,
+    img: cgclogo,
+    school: "Chandigarh Engineering College, Landran",
+    date: "Sept 2021 - June 2025",
+    grade: "87.8%",
+    desc: "I completed my Bachelor's degree in Computer Science (B.Tech) from Chandigarh Engineering College, Landran. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at Chandigarh Engineering College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+    degree: "Bachelor of Technology - B.Tech (Computer Science)",
+  },
+  {
+    id: 2,
+    img: mmlogo,
+    school: "Maharishi Markandeshwar International School, Mullana",
+    date: "Apr 2020 - May 2021",
+    grade: "93.6%",
+    desc: "I completed my class 12 education from Maharishi Markandeshwar International School, Mullana, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+    degree: "CBSE(XII) - PCM with Computer Science",
+  },
+  {
+    id: 3,
+    img: mmlogo,
+    school: "Maharishi Markandeshwar International School, Mullana",
+    date: "Apr 2018 - March 2019",
+    grade: "89.2%",
+    desc: "I completed my class 10 education from Maharishi Markandeshwar International School, Mullana, under the CBSE board, where I studied Science with Computer.",
+    degree: "CBSE(X), Science with Computer Application",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Heart Stroke Prediction",
+    description:
+      "machine learning-based application developed in Python that aims to predict the likelihood of a person suffering a heart stroke based on key health indicators. Leveraging supervised learning algorithms, the project utilizes NumPy and Pandas for efficient data preprocessing, analysis, and feature engineering. The model is trained on relevant medical datasets to ensure accuracy and reliability. A user-friendly Streamlit interface enables real-time input and prediction, making the system accessible for both healthcare professionals and individuals. This project demonstrates the integration of data science and web deployment to support early diagnosis and preventive healthcare decisions.",
+    image: heartStroke,
+    tags: ["Python", "Machine Learning", "Numpy", "Pandas ", "Matplotlib", "Streamlit", "Jupyter"],
+    github: "https://github.com/AbhinavChauhan549/Heart-Stroke-Prediction-ML",
+    webapp: "https://abhinavchauhan549-heart-stroke-prediction-ml-app-bglypd.streamlit.app/",
+  },
+  {
+    id: 1,
+    title: "House Price Prediction",
+    description:
+      "House Price Prediction is a machine learning-based application developed in Python designed to estimate property prices based on key features such as location, age and number of rooms. The project leverages supervised learning algorithms for accurate predictive modeling, with NumPy and Pandas used for data cleaning, preprocessing, and analysis. The trained model provides reliable price estimates by learning patterns from historical real estate data. A user-friendly Streamlit interface enables users to input property details and instantly view predicted prices, making it a practical tool for buyers, sellers, and real estate professionals. This project highlights the effective application of data science and machine learning in the real estate domain.",
+    image: housePrice,
+    tags: ["Python", "Machine Learning", "Numpy", "Pandas ", "Matplotlib", "Streamlit", "Jupyter"],
+    github: "https://github.com/AbhinavChauhan549/House-Price-ML",
+    webapp: "https://house-price-prediction-ml-u4cushuphd5tfzvecvjr2j.streamlit.app/",
+  },
+  {
+    id: 2,
+    title: "Velvet Pour Mojito",
+    description:
+      "Velvet Pour Mojito is a visually captivating frontend landing page designed to showcase a premium mojito brand with elegance and style. Built using React.js, it leverages GSAP (GreenSock Animation Platform) and Framer Motion to deliver smooth, immersive animations and engaging user interactions, creating a luxurious and modern web experience.The landing page highlights the brand’s identity, product visuals, and refreshing aesthetic through fluid transitions, scroll-based effects, and motion design, ensuring a delightful user journey. The combination of React’s component-based architecture with high-performance animation libraries ensures both responsiveness and visual storytelling.",
+    image: mojito,
+    tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+    github: "https://github.com/AbhinavChauhan549/Velvet-pour-mojito",
+    webapp: "https://velvet-pour-mojito.netlify.app/",
+  },
+
+  {
+    id: 3,
+    title: "Tunely : Web App",
+    description:
+      "Tunely is an interactive music streaming web application built using the MERN stack (MongoDB, Express.js, React.js, Node.js). The platform allows users to discover, stream, and share music seamlessly, offering a dynamic and engaging listening experience. It integrates real-time chat functionality powered by Socket.io, enabling users to connect, interact, and share their thoughts instantly while enjoying their favorite tracks.Key features include user authentication, personalized playlists, responsive UI, and real-time messaging, providing a complete social music experience. Tunely showcases the integration of modern web technologies, real-time communication, and scalable backend architecture for enhanced user engagement.",
+    image: tunely,
+    tags: ["HTML", "CSS", "JavaScript", "React Js", "Node.js", "MongoDB", "Express", "JWT", "Mongoose", "Redux"],
+    github: "https://github.com/AbhinavChauhan549/Tunely",
+    webapp: "https://tunely.onrender.com",
+  },
+  {
+    id: 4,
+    title: "Ochi : Frontend Landing Page",
+    description:
+      "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
+    image: Ochi,
+    tags: ["React JS", "Framer Motion", "Locomotive Js", "HTML", "CSS", "JavaScript"],
+    github: "https://github.com/AbhinavChauhan549/Ochi",
+    webapp: "https://abhinavchauhan549.github.io/Ochi/",
+  },
+  {
+    id: 5,
+    title: "Riffly Ecommerce : Web App",
+    description:
+      "Riffly is a full-featured eCommerce web application developed using the MERN stack (MongoDB, Express.js, React.js, Node.js). It provides a seamless online shopping experience with features like product browsing, user authentication, shopping cart management, secure checkout, and order tracking.The platform integrates a modern, responsive UI built with React.js, ensuring smooth navigation and a user-friendly interface. On the backend, Node.js and Express.js handle API logic and server operations, while MongoDB efficiently manages product, user, and order data.Riffly also supports JWT-based authentication, role-based access control, and real-time updates for a robust and secure shopping experience. Designed with scalability and performance in mind, Riffly demonstrates the complete implementation of CRUD operations, RESTful APIs, and state management in a production-ready environment. ",
+    image: riffly,
+    tags: ["HTML", "CSS", "JavaScript", "React Js", "Node.js", "MongoDB", "Express", "JWT", "Mongoose", "Redux"],
+    github: "https://github.com/AbhinavChauhan549/Riffly-Ecommerce",
+    webapp: "https://github.com/AbhinavChauhan549/Riffly-Ecommerce",
+  },
+  {
+    id: 6,
+    title: "AMIX : Web App",
+    description:
+      "AMIX is a comprehensive doctor appointment booking platform developed using the MERN stack (MongoDB, Express.js, React.js, Node.js). The application enables patients to search for doctors, view profiles, book appointments, and manage schedules seamlessly, while doctors can manage availability, approve bookings, and interact with patients efficiently.The platform features a user-friendly interface built with React.js, offering smooth navigation and responsive design. Node.js and Express.js power the backend, handling routing, APIs, and server logic, while MongoDB stores all user, doctor, and appointment data securely.AMIX integrates JWT-based authentication, role-based dashboards for doctors and patients, and real-time appointment status updates, providing a reliable and scalable solution for healthcare scheduling",
+    image: amix,
+    tags: ["HTML", "CSS", "JavaScript", "React Js", "Node.js", "MongoDB", "Express", "JWT", "Mongoose", "Redux"],
+    github: "https://github.com/AbhinavChauhan549/Amix-Backend",
+    webapp: "https://github.com/AbhinavChauhan549/Amix-Backend",
+  },
+  {
+    id: 7,
+    title: "Rock paper scissors Game : Frontend Web App",
+    description:
+      "This project is a modern and responsive landing page crafted using HTML, CSS, and JavaScript. It is designed to provide a visually appealing and engaging user experience, with a focus on simplicity, performance, and interactivity.",
+    image: rockpaperscissor,
+    tags: ["HTML", "CSS", "Javascript"],
+    github: "https://github.com/AbhinavChauhan549/Rock-Paper-Scissor-Game",
+    webapp: "https://abhinavchauhan549.github.io/Rock-Paper-Scissor-Game/",
+  },
+  {
+    id: 8,
+    title: "Tip Calculator",
+    description:
+      "The Tip Calculator is a simple and interactive web application developed using HTML, CSS, and JavaScript that allows users to quickly calculate the tip amount and total bill per person. It provides an intuitive interface where users can input the bill amount, tip percentage, and number of people, instantly displaying accurate results.",
+    image: tip,
+    tags: ["HTML", "CSS", "Javascript"],
+    github: "https://github.com/AbhinavChauhan549/Tip-Calculator",
+    webapp: "https://abhinavchauhan549.github.io/Tip-Calculator/",
+  },
+];  
